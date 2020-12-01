bae876ea7372884d0031c53a1ee15d50eecbc796
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-12-01 12:11:20 -0600

    level fixed

---
+++ frontend/src/services/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+let baseURL;
+
+process.env.NODE_ENV === 'production'
+  ? (baseURL = 'here should be your production endpoint')
+  : (baseURL = 'http://localhost:3000');
+
+const service = axios.create({ withCredentials: true, baseURL });
+
+const MY_SERVICE = {
+  test: async () => {
+    return await service.get('/');
+  },
+  signup: async (user) => {
+    return await SERVICE.post('/signup', user);
+  },
+  login: async (user) => {
+    return await SERVICE.post('/login', user);
+  },
+  logOut: async () => {
+    return await SERVICE.get('/logout');
+  }
+};
+
+export default MY_SERVICE;

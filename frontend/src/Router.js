19c1803c2bb93963e97104ace7923ab9f2d75dc3
ahernandez <albertohernandez_91@yahoo.com>
2020-12-02 19:01:13 -0600

    profile component change

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import NotFound from './components/404/NotFound.js';
-import Profile from './pages/profile'
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/profile" component={Profile} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>

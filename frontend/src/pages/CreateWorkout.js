283d546e637027be1cf1c5877ea02f5da95a01d2
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-12-06 23:09:40 -0600

    create workout page style

--- frontend/src/pages/CreateWorkout.js
+++ frontend/src/pages/CreateWorkout.js
@@ -100,7 +100,7 @@ const CreateWorkout = () => {
   }
   return (
     <>
-       {isModalVisible ? (
+      {isModalVisible ? (
         <Modal
           visible={isModalVisible}
           title="How to perform the exercise"
@@ -111,17 +111,18 @@ const CreateWorkout = () => {
             </Button>,
           ]}
         >
-       <ReactPlayer url={modalVideoUrl} width="100%" height="260px" />
+          <ReactPlayer url={modalVideoUrl} width="100%" height="260px" />
         </Modal>
       ) : null}
 
       <div id="create-workout">
         <div className="left">
           <Row>
-            <Typography.Title className="title" level={1}>
+            <Typography.Title level={1}>
               CREATE A <span>NEW WORKOUT</span>
             </Typography.Title>
           </Row>
+          <br/>
           <Row>
             <Typography.Paragraph>
               You can create your workout form a variaty of exercises, just look
@@ -130,74 +131,85 @@ const CreateWorkout = () => {
           </Row>
 
           {show ? <WorkoutForm exerciseArr={exerciseArr}></WorkoutForm> : null}
-      {show ? (
-        <>
-          {/* <Divider /> */}
-          <Col span={24}>
-            <Form form={form} layout="vertical" onFinish={handleSubmit}>
-              <Form.Item
-                rules={[{ required: true }]}
-                name="name"
-                label="Name your workout"
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item
-                name="exercise"
-                label="Select a difficulty level for your workout according to exercise selection"
-                rules={[{ required: true }]}
-              >
-                <Select>
-                  <Select.Option value="Begginer">Begginer</Select.Option>
-                  <Select.Option value="Intermediate">
-                    Intermediate
-                  </Select.Option>
-                  <Select.Option value="Advanced">Advanced</Select.Option>
-                </Select>
-              </Form.Item>
-              <Button type="primary" block size="middle" htmlType="submit">
-                Create Wrokout
-              </Button>
-            </Form>
-          </Col>
-        </>
-      ) : null}
-      <br />
-      {show ? (
-        <Button
-          type="danger"
-          block
-          size="middle"
-          onClick={() => {
-            setShow(false)
-            setExerciseArr([])
-          }}
-        >
-          Cancel
-        </Button>
-      ) : null}
-      <Row gutter={[16, 16]}>
-        <Col xs={24}>
-          <Typography.Title level={4} style={{ margin: "2rem 0" }}>
-            Select exercises to add
-          </Typography.Title>
-          <Typography.Paragraph ellipsis>
-            You can browse specific exercise by name or even by muscle group
-            i.e., "Lower Body", "Upper Body", "Tricep", "Leg".
-          </Typography.Paragraph>
-          <Typography.Paragraph ellipsis>
-            Select an exercise to see a quick video demonstrating how to perform the movement correctly and decide whether to add it to your workout or not.
-          </Typography.Paragraph>
-
-
-
-
-        </Col>
-      </Row>
+          {show ? (
+            <>
+              <Col span={24}>
+                <Form form={form} layout="vertical" onFinish={handleSubmit}>
+                  <Form.Item
+                    rules={[{ required: true }]}
+                    name="name"
+                    label="Name your workout"
+                  >
+                    <Input />
+                  </Form.Item>
+                  <Form.Item
+                    name="exercise"
+                    label="Select a difficulty level for your workout according to exercise selection"
+                    rules={[{ required: true }]}
+                  >
+                    <Select>
+                      <Select.Option value="Begginer">Begginer</Select.Option>
+                      <Select.Option value="Intermediate">
+                        Intermediate
+                      </Select.Option>
+                      <Select.Option value="Advanced">Advanced</Select.Option>
+                    </Select>
+                  </Form.Item>
+                  <Button type="primary" block size="middle" htmlType="submit">
+                    Create Wrokout
+                  </Button>
+                  <br/>
+                  <br/>
+                  {show ? (
+            <Button
+              danger
+              block
+              size="middle"
+              onClick={() => {
+                setShow(false)
+                setExerciseArr([])
+              }}
+            >
+              Cancel
+            </Button>
+          ) : null}
+                </Form>
+              </Col>
+            </>
+          ) : null}
+          <br />
+          {/* {show ? (
+            <Button
+              type="danger"
+              block
+              size="middle"
+              onClick={() => {
+                setShow(false)
+                setExerciseArr([])
+              }}
+            >
+              Cancel
+            </Button>
+          ) : null} */}
           
         </div>
 
         <div className="right">
+   
+            <Typography.Title level={4}>
+           Let's get started!
+            </Typography.Title>
+  
+          <Typography.Paragraph>
+            <p>
+            You can browse specific exercise by name or even by muscle group
+            i.e., "Lower Body", "Upper Body", "Tricep", "Leg".
+            <br />
+            Select an exercise to see a quick video demonstrating how to perform
+            the movement correctly and decide whether to add it to your workout
+            or not.
+            </p>
+          </Typography.Paragraph>
           <Search
             value={searchQuery}
             onChange={onChange}
@@ -223,8 +235,10 @@ const CreateWorkout = () => {
                         src={item.imageUrl}
                         alt=""
                       />
-                       <div className="text">
-                        <span><p>{item.name}</p></span>
+                      <div className="text">
+                        <span>
+                          <p>{item.name}</p>
+                        </span>
                         <p>{item.muscle_group}</p>
                       </div>
                       <Button
@@ -266,7 +280,9 @@ const CreateWorkout = () => {
                         alt=""
                       />
                       <div className="text">
-                        <span><p>{item.name}</p></span>
+                        <span>
+                          <p>{item.name}</p>
+                        </span>
                         <p>{item.muscle_group}</p>
                       </div>
                       <Button
@@ -299,90 +315,85 @@ const CreateWorkout = () => {
               <></>
             )}
           </Row>
-
-              
-
         </div>
       </div>
     </>
     // <>
-      // {isModalVisible ? (
-      //   <Modal
-      //     visible={isModalVisible}
-      //     title="How to perform the exercise"
-      //     onCancel={handleCancel}
-      //     footer={[
-      //       <Button key="submit" type="primary" onClick={handleOk}>
-      //         Okay
-      //       </Button>,
-      //     ]}
-      //   >
-      //  <ReactPlayer url={modalVideoUrl} width="100%" height="260px" />
-      //   </Modal>
-      // ) : null}
+    // {isModalVisible ? (
+    //   <Modal
+    //     visible={isModalVisible}
+    //     title="How to perform the exercise"
+    //     onCancel={handleCancel}
+    //     footer={[
+    //       <Button key="submit" type="primary" onClick={handleOk}>
+    //         Okay
+    //       </Button>,
+    //     ]}
+    //   >
+    //  <ReactPlayer url={modalVideoUrl} width="100%" height="260px" />
+    //   </Modal>
+    // ) : null}
 
-      // {show ? <WorkoutForm exerciseArr={exerciseArr}></WorkoutForm> : null}
-      // {show ? (
-      //   <>
-      //     {/* <Divider /> */}
-      //     <Col span={24}>
-      //       <Form form={form} layout="vertical" onFinish={handleSubmit}>
-      //         <Form.Item
-      //           rules={[{ required: true }]}
-      //           name="name"
-      //           label="Name your workout"
-      //         >
-      //           <Input />
-      //         </Form.Item>
-      //         <Form.Item
-      //           name="exercise"
-      //           label="Select a difficulty level for your workout according to exercise selection"
-      //           rules={[{ required: true }]}
-      //         >
-      //           <Select>
-      //             <Select.Option value="Begginer">Begginer</Select.Option>
-      //             <Select.Option value="Intermediate">
-      //               Intermediate
-      //             </Select.Option>
-      //             <Select.Option value="Advanced">Advanced</Select.Option>
-      //           </Select>
-      //         </Form.Item>
-      //         <Button type="primary" block size="middle" htmlType="submit">
-      //           Create Wrokout
-      //         </Button>
-      //       </Form>
-      //     </Col>
-      //   </>
-      // ) : null}
-      // <br />
-      // {show ? (
-      //   <Button
-      //     type="danger"
-      //     block
-      //     size="middle"
-      //     onClick={() => {
-      //       setShow(false)
-      //       setExerciseArr([])
-      //     }}
-      //   >
-      //     Cancel
-      //   </Button>
-      // ) : null}
-      // <Row gutter={[16, 16]}>
-      //   <Col xs={24}>
-      //     <Typography.Title level={4} style={{ margin: "2rem 0" }}>
-      //       Select exercises to add
-      //     </Typography.Title>
-      //     <Typography.Paragraph ellipsis>
-      //       You can browse specific exercise by name or even by muscle group
-      //       i.e., "Lower Body", "Upper Body", "Tricep", "Leg".
-      //     </Typography.Paragraph>
-      //     <Typography.Paragraph ellipsis>
-      //       Select an exercise to see a quick video demonstrating how to perform the movement correctly and decide whether to add it to your workout or not.
-      //     </Typography.Paragraph>
+    // {show ? <WorkoutForm exerciseArr={exerciseArr}></WorkoutForm> : null}
+    // {show ? (
+    //   <>
+    //     {/* <Divider /> */}
+    //     <Col span={24}>
+    //       <Form form={form} layout="vertical" onFinish={handleSubmit}>
+    //         <Form.Item
+    //           rules={[{ required: true }]}
+    //           name="name"
+    //           label="Name your workout"
+    //         >
+    //           <Input />
+    //         </Form.Item>
+    //         <Form.Item
+    //           name="exercise"
+    //           label="Select a difficulty level for your workout according to exercise selection"
+    //           rules={[{ required: true }]}
+    //         >
+    //           <Select>
+    //             <Select.Option value="Begginer">Begginer</Select.Option>
+    //             <Select.Option value="Intermediate">
+    //               Intermediate
+    //             </Select.Option>
+    //             <Select.Option value="Advanced">Advanced</Select.Option>
+    //           </Select>
+    //         </Form.Item>
+    //         <Button type="primary" block size="middle" htmlType="submit">
+    //           Create Wrokout
+    //         </Button>
+    //       </Form>
+    //     </Col>
+    //   </>
+    // ) : null}
+    // <br />
+    // {show ? (
+    //   <Button
+    //     type="danger"
+    //     block
+    //     size="middle"
+    //     onClick={() => {
+    //       setShow(false)
+    //       setExerciseArr([])
+    //     }}
+    //   >
+    //     Cancel
+    //   </Button>
+    // ) : null}
+    // <Row gutter={[16, 16]}>
+    //   <Col xs={24}>
+    //     <Typography.Title level={4} style={{ margin: "2rem 0" }}>
+    //       Select exercises to add
+    //     </Typography.Title>
+    //     <Typography.Paragraph ellipsis>
+    //       You can browse specific exercise by name or even by muscle group
+    //       i.e., "Lower Body", "Upper Body", "Tricep", "Leg".
+    //     </Typography.Paragraph>
+    //     <Typography.Paragraph ellipsis>
+    //       Select an exercise to see a quick video demonstrating how to perform the movement correctly and decide whether to add it to your workout or not.
+    //     </Typography.Paragraph>
 
-
-    
     // <Search
     //   value={searchQuery}
     //   onChange={onChange}
@@ -493,71 +504,68 @@ export default CreateWorkout
 
 // <>
 
+//   {show ? <WorkoutForm exerciseArr={exerciseArr}></WorkoutForm> : null}
+//   {show ? (
+//     <>
+//       {/* <Divider /> */}
+//       <Col span={24}>
+//         <Form form={form} layout="vertical" onFinish={handleSubmit}>
+//           <Form.Item
+//             rules={[{ required: true }]}
+//             name="name"
+//             label="Name your workout"
+//           >
+//             <Input />
+//           </Form.Item>
+//           <Form.Item
+//             name="exercise"
+//             label="Select a difficulty level for your workout according to exercise selection"
+//             rules={[{ required: true }]}
+//           >
+//             <Select>
+//               <Select.Option value="Begginer">Begginer</Select.Option>
+//               <Select.Option value="Intermediate">
+//                 Intermediate
+//               </Select.Option>
+//               <Select.Option value="Advanced">Advanced</Select.Option>
+//             </Select>
+//           </Form.Item>
+//           <Button type="primary" block size="middle" htmlType="submit">
+//             Create Wrokout
+//           </Button>
+//         </Form>
+//       </Col>
+//     </>
+//   ) : null}
+//   <br />
+//   {show ? (
+//     <Button
+//       type="danger"
+//       block
+//       size="middle"
+//       onClick={() => {
+//         setShow(false)
+//         setExerciseArr([])
+//       }}
+//     >
+//       Cancel
+//     </Button>
+//   ) : null}
+//   <Row gutter={[16, 16]}>
+//     <Col xs={24}>
+//       <Typography.Title level={4} style={{ margin: "2rem 0" }}>
+//         Select exercises to add
+//       </Typography.Title>
+//       <Typography.Paragraph ellipsis>
+//         You can browse specific exercise by name or even by muscle group
+//         i.e., "Lower Body", "Upper Body", "Tricep", "Leg".
+//       </Typography.Paragraph>
+//       <Typography.Paragraph ellipsis>
+//         Select an exercise to see a quick video demonstrating how to perform the movement correctly and decide whether to add it to your workout or not.
+//       </Typography.Paragraph>
 
-    //   {show ? <WorkoutForm exerciseArr={exerciseArr}></WorkoutForm> : null}
-    //   {show ? (
-    //     <>
-    //       {/* <Divider /> */}
-    //       <Col span={24}>
-    //         <Form form={form} layout="vertical" onFinish={handleSubmit}>
-    //           <Form.Item
-    //             rules={[{ required: true }]}
-    //             name="name"
-    //             label="Name your workout"
-    //           >
-    //             <Input />
-    //           </Form.Item>
-    //           <Form.Item
-    //             name="exercise"
-    //             label="Select a difficulty level for your workout according to exercise selection"
-    //             rules={[{ required: true }]}
-    //           >
-    //             <Select>
-    //               <Select.Option value="Begginer">Begginer</Select.Option>
-    //               <Select.Option value="Intermediate">
-    //                 Intermediate
-    //               </Select.Option>
-    //               <Select.Option value="Advanced">Advanced</Select.Option>
-    //             </Select>
-    //           </Form.Item>
-    //           <Button type="primary" block size="middle" htmlType="submit">
-    //             Create Wrokout
-    //           </Button>
-    //         </Form>
-    //       </Col>
-    //     </>
-    //   ) : null}
-    //   <br />
-    //   {show ? (
-    //     <Button
-    //       type="danger"
-    //       block
-    //       size="middle"
-    //       onClick={() => {
-    //         setShow(false)
-    //         setExerciseArr([])
-    //       }}
-    //     >
-    //       Cancel
-    //     </Button>
-    //   ) : null}
-    //   <Row gutter={[16, 16]}>
-    //     <Col xs={24}>
-    //       <Typography.Title level={4} style={{ margin: "2rem 0" }}>
-    //         Select exercises to add
-    //       </Typography.Title>
-    //       <Typography.Paragraph ellipsis>
-    //         You can browse specific exercise by name or even by muscle group
-    //         i.e., "Lower Body", "Upper Body", "Tricep", "Leg".
-    //       </Typography.Paragraph>
-    //       <Typography.Paragraph ellipsis>
-    //         Select an exercise to see a quick video demonstrating how to perform the movement correctly and decide whether to add it to your workout or not.
-    //       </Typography.Paragraph>
+// </Row>
+//     </Col>
+//   </Row>
 
-
-
-    // </Row>
-    //     </Col>
-    //   </Row>
-
-    // </>
+// </>

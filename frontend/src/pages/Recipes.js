3513dde1369fcae9e06e5fbbffa28a932cc21bce
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-12-08 01:07:54 -0600

    recipes display

--- frontend/src/pages/Recipes.js
+++ frontend/src/pages/Recipes.js
@@ -29,7 +29,7 @@ const Recipies = () => {
   useEffect(() => {
     getRecipies()
   }, [query])
-  
+
   const getRecipesOnce = async () => {
     const { data } = await MY_SERVICE.getRecipesChicken()
     console.log(data)
@@ -42,14 +42,14 @@ const Recipies = () => {
     setRecipes(data.hits)
   }
 
-const updateSearch = (e) => {
-  setSearch(e.target.value)
-  console.log(e.target.value)
-}
+  const updateSearch = (e) => {
+    setSearch(e.target.value)
+    console.log(e.target.value)
+  }
 
-const getSearch = e => {
-  setQuery(search)
-}
+  const getSearch = (e) => {
+    setQuery(search)
+  }
   return (
     <>
       <Search
@@ -59,19 +59,23 @@ const getSearch = e => {
         onSearch={getSearch}
       />
       <Divider />
+      <Row>
       {recipes.map((recipe) => (
         <>
-          <Card
-            key={recipe.recipe.calories}
-            title={recipe.recipe.label}
-            extra={<a href="#">More</a>}
-            style={{ width: 300 }}
-          >
-            <img src={recipe.recipe.image} alt="" />
-            <p>Calories: {recipe.recipe.calories}</p>
-          </Card>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4} key={`${recipe.recipe.label}${recipe.recipe.calories}`}>
+            <Card
+              key={recipe.recipe.calories}
+              title={recipe.recipe.label}
+              extra={<a href="#">More</a>}
+              style={{ width: 300 }}
+            >
+              <img src={recipe.recipe.image} alt="" />
+              <p>Calories: {recipe.recipe.calories}</p>
+            </Card>
+          </Col>
         </>
       ))}
+      </Row>
     </>
   )
 }

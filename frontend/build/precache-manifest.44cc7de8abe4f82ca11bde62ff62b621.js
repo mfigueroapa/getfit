23395f2a514cc271fb6003ac525e48920d26ff78
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-12-09 10:58:07 -0600

    app.js config

---
+++ frontend/build/precache-manifest.44cc7de8abe4f82ca11bde62ff62b621.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebb03411e23c6f6812e00fba23a6c1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee373c16292ed884bb96",
+    "url": "/static/css/14.2c3ec5e2.chunk.css"
+  },
+  {
+    "revision": "f181a2e8745145ebcf76",
+    "url": "/static/css/main.355b9037.chunk.css"
+  },
+  {
+    "revision": "ee373c16292ed884bb96",
+    "url": "/static/js/14.17d15e27.chunk.js"
+  },
+  {
+    "revision": "f181a2e8745145ebcf76",
+    "url": "/static/js/main.9b3a03d9.chunk.js"
+  },
+  {
+    "revision": "b48e9055ebc1d5c1d3f3",
+    "url": "/static/js/reactPlayerDailyMotion.e8a639ae.chunk.js"
+  },
+  {
+    "revision": "5c5bece49e3bb9437ebb",
+    "url": "/static/js/reactPlayerFacebook.a5c534dd.chunk.js"
+  },
+  {
+    "revision": "4912fc8bd80448355b8f",
+    "url": "/static/js/reactPlayerFilePlayer.22c61f52.chunk.js"
+  },
+  {
+    "revision": "ae1f5eed868df900d00a",
+    "url": "/static/js/reactPlayerMixcloud.85a27573.chunk.js"
+  },
+  {
+    "revision": "6d3210399bd19da1bb2f",
+    "url": "/static/js/reactPlayerPreview.72b7aa6b.chunk.js"
+  },
+  {
+    "revision": "a1bb86448cc35cda1516",
+    "url": "/static/js/reactPlayerSoundCloud.5fcbb233.chunk.js"
+  },
+  {
+    "revision": "f2793709c008ec84fbbf",
+    "url": "/static/js/reactPlayerStreamable.a405cdae.chunk.js"
+  },
+  {
+    "revision": "5462dddff3b0697a4c2d",
+    "url": "/static/js/reactPlayerTwitch.580006fb.chunk.js"
+  },
+  {
+    "revision": "73d0578e3371f201a1a1",
+    "url": "/static/js/reactPlayerVidyard.1966ee4c.chunk.js"
+  },
+  {
+    "revision": "27b586fc14bdacc81991",
+    "url": "/static/js/reactPlayerVimeo.36a842c8.chunk.js"
+  },
+  {
+    "revision": "bf15c8c72c456dbb4f9f",
+    "url": "/static/js/reactPlayerWistia.b4a2d9e2.chunk.js"
+  },
+  {
+    "revision": "3d992b90870f371d92d9",
+    "url": "/static/js/reactPlayerYouTube.b7197e2d.chunk.js"
+  },
+  {
+    "revision": "986e9ef0eb17e36890ae",
+    "url": "/static/js/runtime-main.71d3bb0a.js"
+  },
+  {
+    "revision": "2687cd39b79e2386e834dbbd4581bab1",
+    "url": "/static/media/feed.2687cd39.jpg"
+  },
+  {
+    "revision": "f169ad4302167c338fbe6ee4445f57b6",
+    "url": "/static/media/hero.f169ad43.jpg"
+  },
+  {
+    "revision": "6d2e1841cd771584c0433fa447144c6e",
+    "url": "/static/media/logo.6d2e1841.svg"
+  },
+  {
+    "revision": "6bfe5b57b7c615edd2eaa7abfd7492e2",
+    "url": "/static/media/promo1.6bfe5b57.jpg"
+  },
+  {
+    "revision": "638ed27d2c7ed66b54bf200235171a9a",
+    "url": "/static/media/promo2.638ed27d.jpg"
+  }
+]);

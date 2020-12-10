6d2a520cf19e2d05cec12ab423551b0e117d9190
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-12-10 17:33:12 -0600

    / fix

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -25,7 +25,7 @@ const MY_SERVICE = {
     return await service.get('/profile')
   },
   updateUser: async (id, user) => {
-    return await service.put(`user/update/${id}`, user)
+    return await service.put(`/user/update/${id}`, user)
     // return await service.post(`user/update/${id}`, user)
   },
   editInfo: async (userData) => {
@@ -33,15 +33,15 @@ const MY_SERVICE = {
     // return await service.post("/user/edit-info", userData)
   },
   deleteUser: async (id) => {
-    return await service.delete(`user/delete/${id}`)
+    return await service.delete(`/user/delete/${id}`)
     // return await service.post(`user/delete/${id}`)
   },
   updatePic: async (id, image) => {
-    return await service.put(`user/update-pic/${id}`, image)
+    return await service.put(`/user/update-pic/${id}`, image)
     // return await service.post(`user/update-pic/${id}`, image)
   },
   addWorkoutToFavorites: async (workout) => {
-    return await service.post(`user/save-workout-to-favorites`, workout)
+    return await service.post(`/user/save-workout-to-favorites`, workout)
   },
   getExercises: async () => {
     return await service.get("/api/getExercises")
@@ -53,7 +53,7 @@ const MY_SERVICE = {
     return await service.get(`/api/workouts/${id}`)
   },
   createWorkout: async (workoutInfo) => {
-    return await service.post('user/create-workout', workoutInfo)
+    return await service.post('/user/create-workout', workoutInfo)
   },
   getRecipesChicken: async () => {
     return await service.get(`https://api.edamam.com/search?q=chicken&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}`)
@@ -62,10 +62,10 @@ const MY_SERVICE = {
     return await service.get(`https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}`)
   },
   getFavWorkouts: async () => {
-    return await service.get(`user/saved-workouts`)
+    return await service.get(`/user/saved-workouts`)
   },
   removeFavorite: async (workout) => {
-    return await service.post('user/remove-favorite', workout)
+    return await service.post('/user/remove-favorite', workout)
   }
 };
 
